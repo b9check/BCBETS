e2cb07d646f63593b83e7241533a806b48f42d1a
b9check <b9check@gmail.com>
2024-02-28 16:07:18 -0500

    Big folder setup change

--- csv_creation.py
+++ csv_creation.py
@@ -6,5 +6,5 @@ from compare_odds import get_DK_bets_analysis
 current_metrics, rounded_metrics = get_current_metrics()
 DK_analysis = get_DK_bets_analysis()
 
-rounded_metrics.to_csv('rounded_metrics.csv', index=False)
-DK_analysis.to_csv('DK_analysis.csv', index=False)
+rounded_metrics.to_csv('C:\\Users\\brian\\OneDrive\\Desktop\\SportsAnalytics\\NBAWins\\Website\\server\\rounded_metrics.csv', index=False)
+DK_analysis.to_csv('C:\\Users\\brian\\OneDrive\\Desktop\\SportsAnalytics\\NBAWins\\Website\\server\\DK_analysis.csv', index=False)
